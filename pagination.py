27954e2a050a661775313f599b42ed0e573a9f86
Luis Soares <luis.soares@aspiretechnology.com>
2022-12-17 19:53:23 +0000

    push test

---
+++ pagination.py
@@ -0,0 +1,76 @@
+from math import floor
+
+# SUMMARY
+
+# The numbers that are to be printed in the end, will be added to the set.
+# I choose the set because the boundaries and around will sometimes overlap,
+# which using a list would mean I would had repeated page values.
+
+class Pagination:
+    def __init__(self, current_page: int, total_pages: int, boundaries: int,
+                 around: int):
+
+        self.current_page: int = current_page
+        self.total_pages: int = total_pages
+        self.boundaries: int = boundaries
+        self.around: int = around
+        self.page_container: set = set()
+
+    def _add_elements(self, start_idx: int, end_idx: int):
+        for page in range(start_idx, end_idx + 1):
+            self.page_container.add(page)
+
+    def exec_boundaries(self, start: bool = True):
+        if start:
+            self._add_elements(1, self.boundaries)
+            return
+        self._add_elements(self.total_pages-self.boundaries+1, self.total_pages)
+
+    def exec_around(self, backwards: bool = True):
+        if backwards:
+            self._add_elements(self.current_page-self.around, self.current_page)
+            return
+        self._add_elements(self.current_page+1, self.current_page + self.around)
+
+    def print_pagination(self):
+        self.page_container.add(self.current_page)
+        self.page_container = sorted(self.page_container)
+        for idx, n in enumerate(self.page_container):
+            if idx == 0:
+                print(n, end=" ")
+                continue
+            elif self.page_container[idx] - self.page_container[idx-1] > 1:
+                print("...", end=" ")
+            print(n, end=" ")
+
+    def validation(self):
+        if not all([
+            isinstance(self.current_page, int), isinstance(self.around, int),
+            isinstance(self.boundaries, int), isinstance(self.total_pages, int)]):
+            raise Exception("Not an integer")
+
+        elif not all([self.current_page > 0, self.around >= 0, self.boundaries >= 0, self.total_pages > 0]):
+            raise Exception("Properties have values below 0 or total pages are 0")
+
+        elif self.current_page > self.total_pages:
+            raise Exception("Current page is bigger than the total number of pages")
+
+        elif self.boundaries > floor(self.total_pages/2):
+            raise Exception(f"No space left for boundaries. Total pages: {self.total_pages}, Boundaries: {self.boundaries}")
+
+        elif self.current_page-self.around < 1 or self.current_page + self.around > self.total_pages:
+            raise Exception("Around value goes below or upwards delimitations")
+
+
+    def execute(self):
+        self.validation()
+        self.exec_boundaries()
+        self.exec_boundaries(False)
+        self.exec_around()
+        self.exec_around(False)
+        self.print_pagination()
+
+
+if __name__ == "__main__":
+    p = Pagination(30, 40, 21, 3)
+    p.execute()
---
+++ unit_test.py
@@ -0,0 +1,70 @@
+from pagination import Pagination
+import unittest
+
+
+class TestPagination(unittest.TestCase):
+
+    def test_current_page_bigger_than_total_pages(self):
+        p = Pagination(current_page=10, total_pages=2, boundaries=3, around=4)
+        with self.assertRaises(Exception):
+            p.execute()
+
+    def test_property_not_integer(self):
+        p = Pagination(current_page="asd", total_pages=2, boundaries=3, around=4)
+        with self.assertRaises(Exception):
+            p.execute()
+
+    def test_big_around(self):
+        p = Pagination(current_page=5, total_pages=11, boundaries=0, around=6)
+        with self.assertRaises(Exception):
+            p.execute()
+    
+    def test_total_pages_zero(self):
+        p = Pagination(current_page=5, total_pages=0, boundaries=1, around=1)
+        with self.assertRaises(Exception):
+            p.execute()
+
+    def test_big_boundaries(self):
+        p = Pagination(current_page=1, total_pages=11, boundaries=6, around=0)
+        with self.assertRaises(Exception):
+            p.execute()
+    
+    def test_negative_current_page(self):
+        p1 = Pagination(current_page=-1, total_pages=5, boundaries=0, around=0)
+
+        with self.assertRaises(Exception):
+            p1.execute()
+    
+    def test_negative_total_pages(self):
+        p2 = Pagination(current_page=1, total_pages=-1, boundaries=0, around=0)
+        with self.assertRaises(Exception):
+            p2.execute()
+
+    def test_negative_boundaries(self):
+        p3 = Pagination(current_page=1, total_pages=5, boundaries=-1, around=0)
+        with self.assertRaises(Exception):
+            p3.execute()
+
+    def test_negative_around(self):
+        p4 = Pagination(current_page=1, total_pages=5, boundaries=6, around=-1)
+        with self.assertRaises(Exception):
+            p4.execute()
+
+    def test_double_three_dots(self):
+        p1 = Pagination(10, 50, 2, 2)
+        p1.execute()
+        self.assertEqual(p1.page_container, [1, 2, 8, 9, 10, 11, 12, 49, 50])
+
+    def test_big_numbers(self):
+        p1 = Pagination(15000, 30000, 3, 6)
+        p1.execute()
+        self.assertEqual(p1.page_container, [1, 2, 3, 14994, 14995, 14996,
+                                             14997, 14998, 14999, 15000, 15001,
+                                             15002, 15003, 15004, 15005, 15006,
+                                             29998, 29999, 30000])
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
